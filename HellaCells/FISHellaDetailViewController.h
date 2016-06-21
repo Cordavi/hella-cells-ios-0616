80a8598a3f673c041691c89bddb9aa045454e19a
Cordavi <michaelamundsen@gmail.com>
2016-06-21 16:28:42 -0400

    Completed lab to create UITableView and detail cell view

---
+++ HellaCells/FISHellaDetailViewController.h
@@ -0,0 +1,14 @@
+//
+//  FISHellaDetailViewController.h
+//  HellaCells
+//
+//  Created by Michael Amundsen on 6/21/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISHellaDetailViewController : UIViewController
+@property (strong, nonatomic) UITableViewCell *cell;
+
+@end
